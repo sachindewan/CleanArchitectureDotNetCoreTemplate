9a6ea57c3d9607c11d3247c5058a26fc58a81ca4
Sachin Kumar <sachindewan12@gmail.com>
2022-11-17 00:43:52 -0600

    Add files via upload

---
+++ CleanArchitecture.WebApi1.Infrastructure.Shared/Common/MediatorExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.WebApi1.Domain.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.WebApi1.Infrastructure.Shared.Common
+{
+    public static class MediatorExtensions
+    {
+        public static async Task DispatchDomainEvents(this IMediator mediator, DbContext context)
+        {
+            var entities = context.ChangeTracker
+                .Entries<BaseEntity>()
+                .Where(e => e.Entity.DomainEvents.Any())
+                .Select(e => e.Entity);
+
+            var domainEvents = entities
+                .SelectMany(e => e.DomainEvents)
+                .ToList();
+
+            entities.ToList().ForEach(e => e.ClearDomainEvents());
+
+            foreach (var domainEvent in domainEvents)
+                await mediator.Publish(domainEvent);
+        }
+    }
+}
