1a9f9eb2c63e7145e80170d8ff2d3f2b27e3d97c
Sachin Kumar <sachindewan12@gmail.com>
2023-07-04 22:50:44 -0600

    Add files via upload

---
+++ CleanArchitecture.WebApi1.Domain/Events/ProductCompletedEvent.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.WebApi1.Domain.Common;
+using CleanArchitecture.WebApi1.Domain.Entities;
+
+namespace CleanArchitecture.WebApi1.Domain.Events
+{
+    public class ProductCompletedEvent:BaseEvent
+    {
+        public Product Product { get; }
+        public ProductCompletedEvent(Product product)
+        {
+            Product = product;
+        }
+    }
+}
---
+++ CleanArchitecture.WebApi1.Domain/Events/ProductDeletedEvent.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.WebApi1.Domain.Common;
+using CleanArchitecture.WebApi1.Domain.Entities;
+
+namespace CleanArchitecture.WebApi1.Domain.Events
+{
+    public class ProductDeletedEvent:BaseEvent
+    {
+        public Product Product { get; }
+        public ProductDeletedEvent(Product product)
+        {
+            Product = product;
+        }
+    }
+}
